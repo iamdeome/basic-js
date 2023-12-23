3a8d497d1ae651cec8c594a6922613d95949a60a
John Doe <2ivShr2!>
2023-12-23 22:55:25 +0100

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let name = false;
+  
+  if (Array.isArray(members)) {
+    name = members
+    .filter(member => typeof member === 'string')
+    .map(element => element.trim()[0].toUpperCase())
+    .sort()
+    .join('');
+  };
+  
+  return name;
 }
 
 module.exports = {
