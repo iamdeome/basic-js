3159e9e4360638ac0129356228008b0b6b8941e4
John Doe <2ivShr2!>
2023-12-25 17:05:30 +0100

    feat: implement extended-repeater

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str.length === 1 || str === '') {
+    return str;
+  }
+
+  let result = '';
+  let count = 1;
+
+  for (let i = 1; i < str.length; i += 1) {
+    if (str[i - 1] === str[i]) {
+      count += 1;
+    } else {
+      result += (count > 1 ? count : '') + str[i - 1];
+      count = 1;
+    }
+  }
+  result += (count > 1 ? count : '') + str[str.length - 1];
+
+  return result;
 }
 
 module.exports = {
